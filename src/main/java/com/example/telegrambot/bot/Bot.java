f0ab96484e76aa6e9b8a7e60b79c654f4966aed6
Dmitriy-684 <dmitriyegorov111@mail.ru>
2024-05-27 23:59:24 +0300

    added simple logging

--- src/main/java/com/example/telegrambot/bot/Bot.java
+++ src/main/java/com/example/telegrambot/bot/Bot.java
@@ -1,2 +1,64 @@
-package com.example.telegrambot.bot;public class Bot {
+package com.example.telegrambot.bot;
+
+import com.example.telegrambot.config.BotConfig;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Chat;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@EqualsAndHashCode(callSuper = true)
+@Service
+@Data
+@Slf4j
+public class Bot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            Message message = update.getMessage();
+            Chat chat = message.getChat();
+            String messageText = message.getText();
+            long chatId = message.getChatId();
+            switch (messageText){
+                case "/start" -> startCommandReceived(chatId, chat.getFirstName());
+                default -> sendMessage(chatId, "Unrecognized command");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String userName){
+        String text = "Hi, " + userName + "!";
+        sendMessage(chatId, text);
+        log.info("Replied to user " + userName);
+    }
+
+    private void sendMessage(long chatId, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(String.valueOf(chatId));
+        sendMessage.setText(text);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotToken(){
+        return config.token;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.name;
+    }
+
 }
--- src/main/java/com/example/telegrambot/config/BotConfig.java
+++ src/main/java/com/example/telegrambot/config/BotConfig.java
@@ -1,2 +1,18 @@
-package com.example.telegrambot.config;public class BotConfig {
+package com.example.telegrambot.config;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@Data
+public class BotConfig{
+
+    @Value("${bot.name}")
+    public String name;
+
+    @Value("${bot.token}")
+    public String token;
+
 }

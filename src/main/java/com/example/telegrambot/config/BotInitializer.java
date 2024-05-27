4407225f5c7d206448c8059cd061a3b2e80e84df
Dmitriy-684 <dmitriyegorov111@mail.ru>
2024-05-28 00:25:49 +0300

    added left menu bar

--- src/main/java/com/example/telegrambot/config/BotInitializer.java
+++ src/main/java/com/example/telegrambot/config/BotInitializer.java
@@ -1,16 +1,17 @@
 package com.example.telegrambot.config;
 
-import com.example.telegrambot.bot.Bot;
+import com.example.telegrambot.service.Bot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-@Service
+@Component
 @Slf4j
 public class BotInitializer {
 
--- src/main/java/com/example/telegrambot/service/Bot.java
+++ src/main/java/com/example/telegrambot/service/Bot.java
@@ -1,4 +1,4 @@
-package com.example.telegrambot.bot;
+package com.example.telegrambot.service;
 
 import com.example.telegrambot.config.BotConfig;
 import lombok.Data;
@@ -6,19 +6,37 @@ import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Service
-@Data
 @Slf4j
 public class Bot extends TelegramLongPollingBot {
 
     private final BotConfig config;
+
+    public Bot(BotConfig config){
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get a welcome message"));
+        listOfCommands.add(new BotCommand("/help", "help"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e){
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
 
     @Override
     public void onUpdateReceived(Update update) {
